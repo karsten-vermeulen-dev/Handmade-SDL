c8bbc1fb68dac38a847a15d3115bcde2b725af2e
Karsten Vermeulen <karsten.vermeulen.dev@gmail.com>
2024-09-29 17:07:54 +0100

    Add extra setters and a function to visualize bounding box

--- src/BoxCollider.cpp
+++ src/BoxCollider.cpp
@@ -1,5 +1,6 @@
-#include <algorithm>
+#include <SDL.h>
 #include "BoxCollider.h"
+#include "Screen.h"
 
 //======================================================================================================
 void BoxCollider::SetPosition(int x, int y)
@@ -8,10 +9,20 @@ void BoxCollider::SetPosition(int x, int y)
 	position.y = y;
 }
 //======================================================================================================
+void BoxCollider::SetPosition(const Vector<int>& position)
+{
+	this->position = position;
+}
+//======================================================================================================
 void BoxCollider::SetDimension(int width, int height)
 {
 	dimension.x = width;
 	dimension.y = height;
+}
+//======================================================================================================
+void BoxCollider::SetDimension(const Vector<int>& dimension)
+{
+	this->dimension = dimension;
 }
 //======================================================================================================
 void BoxCollider::Update()
@@ -26,4 +37,16 @@ bool BoxCollider::IsColliding(const BoxCollider& secondBox) const
 {
 	return ((max.x > secondBox.min.x && secondBox.max.x > min.x) &&
 		(max.y > secondBox.min.y && secondBox.max.y > min.y));
+}
+//======================================================================================================
+void BoxCollider::Render()
+{
+	auto renderer = Screen::Instance()->GetRenderer();
+	auto boundingBox = SDL_Rect{ position.x, position.y, dimension.x, dimension.y };
+
+	//NOTE: This code will eventually reside somewhere more global (Utility or Screen class?)
+	//Until the next big update, we render a primitive quad here to visualize the bounding box 
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 125);
+	SDL_RenderFillRect(renderer, &boundingBox);
 }
--- src/BoxCollider.h
+++ src/BoxCollider.h
@@ -7,16 +7,22 @@ class BoxCollider
 public:
 
 	void SetPosition(int x, int y);
+	void SetPosition(const Vector<int>& position);
+
 	void SetDimension(int width, int height);
+	void SetDimension(const Vector<int>& dimension);
 
 	void Update();
 	bool IsColliding(const BoxCollider& secondBox) const;
+
+	//Intended for debugging only
+	void Render();
 
 private:
 
 	Vector<int> min;
 	Vector<int> max;
 	Vector<int> position;
-	Vector<int> dimension{ 1, 1 };
+	Vector<int> dimension;
 
 };
